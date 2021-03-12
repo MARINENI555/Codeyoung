acd2d5f9031ea2ceea798ff34e4c2d863c5ce706
MARINENI555 <53974153+MARINENI555@users.noreply.github.com>
2021-03-12 21:16:49 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Converter from "./Converter";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Converter />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
